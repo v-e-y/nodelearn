0d92b44746bb2fa6040e740d89052ba76244b833
vey <v-e-y@outlook.com>
2016-01-17 16:43:40 +0300

    Dance with buben around  fs and path

--- files.js
+++ files.js
@@ -1,32 +1,34 @@
 "use strict";
 var fs = require("fs");
+var path = require('path');
 
 function Files(name){
     this.name = name.toLowerCase().trim().toString();
     // Минимальная длина имени файла.
-    // Цыфры выдуманны и не счем не связанны.
+    // Цыфры выдуманны и не с чем не связанны.
     this.minNameLength = 3;
     // Максимальная длина имени файла.
     // Цыфры выдуманны и не счем не связанны.
     this.maxNameLength = 63;
-    // Масив где хранятся имена файлов.
+    // Массив где хранятся имена существующих файлов.
     this.fileNamesDataBase = [];
-    // Папка для франения файлов.
-    this.filesDirectory = "public";
+    // Папка для хранения файлов.
+    this.filesDirectory = __dirname + "\\public";
 
-    // Поверяем существование директории.
+    // Поверяем существование директории/папки.
     // TODO Найти как правильно проверить существование папки
+    // TODO Разобраться как работает .readdirSync (почему не возвращает список файлов)
+    // TODO .readdir[Sync] возвращает список файло в дироктории, что позволит оптемищировать класс.
     fs.readdir(this.filesDirectory, function(err){
         if (err){
             // Если директории нет, создаем её.
-            fs.mkdir("publik", function(err){
+            fs.mkdirSync("publik", function(err){
                 if (err) {
                     throw new Error("Немогу создать папку");
                 }
-            })
+            });
         }
     });
-
 }
 
 // Проверяем уникальность имени файла.
@@ -34,7 +36,7 @@ Files.prototype.checkNameUniqu = function(){
     for (var i = 0; i < this.fileNamesDataBase.length; i++){
         var nameInBase = this.fileNamesDataBase[i];
         if (this.name === nameInBase){
-            return new Error("Такое имя файла уже есть в базе.");
+            throw new Error("Такое имя файла уже есть в базе.");
         }
     }
 };
@@ -43,7 +45,7 @@ Files.prototype.checkNameUniqu = function(){
 // в классе Files, в свойствах minNameLength и maxNameLength.
 Files.prototype.checkFilesNameLength = function() {
     if (this.name.length < this.minNameLength || this.name.length > this.maxNameLength) {
-        return new Error("Длина имени файла должна быть от 3 до 63 символов.");
+        throw new Error("Длина имени файла должна быть от 3 до 63 символов.");
     }
 };
 
